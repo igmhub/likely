2a6ae83fc123d944048861bcf7d8d2d6e6f5e5e1
David Kirkby <dkirkby@uci.edu>
2011-05-21 07:13:15 -0700

    move likelytest to no-install category, add program options parsing skeleton

--- src/likelytest.cc
+++ src/likelytest.cc
@@ -1,14 +1,44 @@
 // Created 20-May-2011 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+// A test program for studying likelihood analysis methods.
 
 #include "likely/likely.h"
+
+#include "boost/program_options.hpp"
 
 #include <iostream>
 
 namespace test = likely::test;
+namespace po = boost::program_options;
 
-int main(int argc, char *argv) {
+int main(int argc, char **argv) {
+    
+    // Configure command-line option processing
+    po::options_description cli("Likelihood analysis test program");
+    cli.add_options()
+        ("help,h", "Prints this info and exits.")
+        ("verbose", "Prints additional information.")
+        ;
+
+    // do the command line parsing now
+    po::variables_map vm;
+    try {
+        po::store(po::parse_command_line(argc, argv, cli), vm);
+        po::notify(vm);
+    }
+    catch(std::exception const &e) {
+        std::cerr << "Unable to parse command line options: " << e.what() << std::endl;
+        return -1;
+    }
+    if(vm.count("help")) {
+        std::cout << cli << std::endl;
+        return 1;
+    }
+    bool verbose(vm.count("verbose"));
+
     test::TestLikelihood likelyfn(3,1,-0.75,0.25);
     std::vector<double> params(3,0);
     params[1] = 1;
     std::cout << likelyfn(params) << std::endl;
+
+    return 0;
 }
