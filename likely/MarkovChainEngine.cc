6433b59f9f38b8000e38f79c99b7adbe422d0bfa
David Kirkby <dkirkby@uci.edu>
2011-05-24 16:51:27 -0700

    add skeleton MCMC engine

---
+++ likely/MarkovChainEngine.cc
@@ -0,0 +1,9 @@
+// Created 24-May-2011 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "likely/MarkovChainEngine.h"
+
+namespace local = likely;
+
+local::MarkovChainEngine::MarkovChainEngine() { }
+
+local::MarkovChainEngine::~MarkovChainEngine() { }
---
+++ likely/MarkovChainEngine.h
@@ -0,0 +1,15 @@
+// Created 24-May-2011 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#ifndef LIKELY_MARKOV_CHAIN_ENGINE
+#define LIKELY_MARKOV_CHAIN_ENGINE
+
+namespace likely {
+	class MarkovChainEngine {
+	public:
+		MarkovChainEngine();
+		virtual ~MarkovChainEngine();
+	private:
+	}; // MarkovChainEngine
+} // likely
+
+#endif // LIKELY_MARKOV_CHAIN_ENGINE
