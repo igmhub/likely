6cd7f163d800d11ea0ce685548aaac35a7d256f7
David Kirkby <dkirkby@uci.edu>
2011-12-23 20:21:34 -0800

    Implement caching and calculation of datacube partial derivatives.

--- likely/TriCubicInterpolator.cc
+++ likely/TriCubicInterpolator.cc
@@ -3,10 +3,12 @@
 #include "likely/TriCubicInterpolator.h"
 #include "likely/RuntimeError.h"
 
+#include <cmath>
+
 namespace local = likely;
 
 local::TriCubicInterpolator::TriCubicInterpolator(DataCube data, int n1, int n2, int n3)
-: _data(data), _n1(n1), _n2(n2), _n3(n3)
+: _data(data), _n1(n1), _n2(n2), _n3(n3), _initialized(false)
 {
     if(_n2 == 0 && _n3 == 0) {
         _n3 = _n2 = _n1;
@@ -15,3 +17,107 @@ local::TriCubicInterpolator::TriCubicInterpolator(DataCube data, int n1, int n2,
 }
 
 local::TriCubicInterpolator::~TriCubicInterpolator() { }
+
+double local::TriCubicInterpolator::operator()(double x, double y, double z) const {
+    // Map x,y,z to a point dx,dy,dz in the unit cube [0,1)^3
+    double dx(std::fmod(x,1)), dy(std::fmod(y,1)), dz(std::fmod(z,1));
+    if(dx < 0) dx += 1;
+    if(dy < 0) dy += 1;
+    if(dz < 0) dz += 1;
+    // Calculate the corresponding lower-bound grid indices.
+    int xi = (int)std::floor(dx*_n1);
+    int yi = (int)std::floor(dy*_n2);
+    int zi = (int)std::floor(dz*_n3);
+    // Check if we can re-use coefficients from the last interpolation.
+    if(!_initialized || xi != _i1 || yi != _i2 || zi != _i3) {
+		double fval[8] = {
+		    _data[_index(xi,yi,zi)],_data[_index(xi+1,yi,zi)],_data[_index(xi,yi+1,zi)],
+		    _data[_index(xi+1,yi+1,zi)],_data[_index(xi,yi,zi+1)],_data[_index(xi+1,yi,zi+1)],
+		    _data[_index(xi,yi+1,zi+1)],_data[_index(xi+1,yi+1,zi+1)]
+		};
+		double dfdxval[8] = {
+		    0.5*(_data[_index(xi+1,yi,zi)]-_data[_index(xi-1,yi,zi)]),
+		    0.5*(_data[_index(xi+2,yi,zi)]-_data[_index(xi,yi,zi)]),
+			0.5*(_data[_index(xi+1,yi+1,zi)]-_data[_index(xi-1,yi+1,zi)]),
+			0.5*(_data[_index(xi+2,yi+1,zi)]-_data[_index(xi,yi+1,zi)]),
+			0.5*(_data[_index(xi+1,yi,zi+1)]-_data[_index(xi-1,yi,zi+1)]),
+			0.5*(_data[_index(xi+2,yi,zi+1)]-_data[_index(xi,yi,zi+1)]),
+			0.5*(_data[_index(xi+1,yi+1,zi+1)]-_data[_index(xi-1,yi+1,zi+1)]),
+			0.5*(_data[_index(xi+2,yi+1,zi+1)]-_data[_index(xi,yi+1,zi+1)])
+		};
+		double dfdyval[8] = {
+		    0.5*(_data[_index(xi,yi+1,zi)]-_data[_index(xi,yi-1,zi)]),
+		    0.5*(_data[_index(xi+1,yi+1,zi)]-_data[_index(xi+1,yi-1,zi)]),
+			0.5*(_data[_index(xi,yi+2,zi)]-_data[_index(xi,yi,zi)]),
+			0.5*(_data[_index(xi+1,yi+2,zi)]-_data[_index(xi+1,yi,zi)]),
+			0.5*(_data[_index(xi,yi+1,zi+1)]-_data[_index(xi,yi-1,zi+1)]),
+			0.5*(_data[_index(xi+1,yi+1,zi+1)]-_data[_index(xi+1,yi-1,zi+1)]),
+			0.5*(_data[_index(xi,yi+2,zi+1)]-_data[_index(xi,yi,zi+1)]),
+			0.5*(_data[_index(xi+1,yi+2,zi+1)]-_data[_index(xi+1,yi,zi+1)])
+		};
+		double dfdzval[8] = {
+		    0.5*(_data[_index(xi,yi,zi+1)]-_data[_index(xi,yi,zi-1)]),
+		    0.5*(_data[_index(xi+1,yi,zi+1)]-_data[_index(xi+1,yi,zi-1)]),
+			0.5*(_data[_index(xi,yi+1,zi+1)]-_data[_index(xi,yi+1,zi-1)]),
+			0.5*(_data[_index(xi+1,yi+1,zi+1)]-_data[_index(xi+1,yi+1,zi-1)]),
+			0.5*(_data[_index(xi,yi,zi+2)]-_data[_index(xi,yi,zi)]),
+			0.5*(_data[_index(xi+1,yi,zi+2)]-_data[_index(xi+1,yi,zi)]),
+			0.5*(_data[_index(xi,yi+1,zi+2)]-_data[_index(xi,yi+1,zi)]),
+			0.25*(_data[_index(xi+1,yi+1,zi+2)]-_data[_index(xi+1,yi+1,zi)])
+		};
+		double d2fdxdyval[8] = {
+		    0.25*(_data[_index(xi+1,yi+1,zi)]-_data[_index(xi-1,yi+1,zi)]-_data[_index(xi+1,yi-1,zi)]+_data[_index(xi-1,yi-1,zi)]),
+			0.25*(_data[_index(xi+2,yi+1,zi)]-_data[_index(xi,yi+1,zi)]-_data[_index(xi+2,yi-1,zi)]+_data[_index(xi,yi-1,zi)]),
+			0.25*(_data[_index(xi+1,yi+2,zi)]-_data[_index(xi-1,yi+2,zi)]-_data[_index(xi+1,yi,zi)]+_data[_index(xi-1,yi,zi)]),
+			0.25*(_data[_index(xi+2,yi+2,zi)]-_data[_index(xi,yi+2,zi)]-_data[_index(xi+2,yi,zi)]+_data[_index(xi,yi,zi)]),
+			0.25*(_data[_index(xi+1,yi+1,zi+1)]-_data[_index(xi-1,yi+1,zi+1)]-_data[_index(xi+1,yi-1,zi+1)]+_data[_index(xi-1,yi-1,zi+1)]),
+			0.25*(_data[_index(xi+2,yi+1,zi+1)]-_data[_index(xi,yi+1,zi+1)]-_data[_index(xi+2,yi-1,zi+1)]+_data[_index(xi,yi-1,zi+1)]),
+			0.25*(_data[_index(xi+1,yi+2,zi+1)]-_data[_index(xi-1,yi+2,zi+1)]-_data[_index(xi+1,yi,zi+1)]+_data[_index(xi-1,yi,zi+1)]),
+			0.25*(_data[_index(xi+2,yi+2,zi+1)]-_data[_index(xi,yi+2,zi+1)]-_data[_index(xi+2,yi,zi+1)]+_data[_index(xi,yi,zi+1)])
+        };
+		double d2fdxdzval[8] = {
+		    0.25*(_data[_index(xi+1,yi,zi+1)]-_data[_index(xi-1,yi,zi+1)]-_data[_index(xi+1,yi,zi-1)]+_data[_index(xi-1,yi,zi-1)]),
+			0.25*(_data[_index(xi+2,yi,zi+1)]-_data[_index(xi,yi,zi+1)]-_data[_index(xi+2,yi,zi-1)]+_data[_index(xi,yi,zi-1)]),
+			0.25*(_data[_index(xi+1,yi+1,zi+1)]-_data[_index(xi-1,yi+1,zi+1)]-_data[_index(xi+1,yi+1,zi-1)]+_data[_index(xi-1,yi+1,zi-1)]),
+			0.25*(_data[_index(xi+2,yi+1,zi+1)]-_data[_index(xi,yi+1,zi+1)]-_data[_index(xi+2,yi+1,zi-1)]+_data[_index(xi,yi+1,zi-1)]),
+			0.25*(_data[_index(xi+1,yi,zi+2)]-_data[_index(xi-1,yi,zi+2)]-_data[_index(xi+1,yi,zi)]+_data[_index(xi-1,yi,zi)]),
+			0.25*(_data[_index(xi+2,yi,zi+2)]-_data[_index(xi,yi,zi+2)]-_data[_index(xi+2,yi,zi)]+_data[_index(xi,yi,zi)]),
+			0.25*(_data[_index(xi+1,yi+1,zi+2)]-_data[_index(xi-1,yi+1,zi+2)]-_data[_index(xi+1,yi+1,zi)]+_data[_index(xi-1,yi+1,zi)]),
+			0.25*(_data[_index(xi+2,yi+1,zi+2)]-_data[_index(xi,yi+1,zi+2)]-_data[_index(xi+2,yi+1,zi)]+_data[_index(xi,yi+1,zi)])
+		};
+		double d2fdydzval[8] = {
+		    0.25*(_data[_index(xi,yi+1,zi+1)]-_data[_index(xi,yi-1,zi+1)]-_data[_index(xi,yi+1,zi-1)]+_data[_index(xi,yi-1,zi-1)]),
+			0.25*(_data[_index(xi+1,yi+1,zi+1)]-_data[_index(xi+1,yi-1,zi+1)]-_data[_index(xi+1,yi+1,zi-1)]+_data[_index(xi+1,yi-1,zi-1)]),
+			0.25*(_data[_index(xi,yi+2,zi+1)]-_data[_index(xi,yi,zi+1)]-_data[_index(xi,yi+2,zi-1)]+_data[_index(xi,yi,zi-1)]),
+			0.25*(_data[_index(xi+1,yi+2,zi+1)]-_data[_index(xi+1,yi,zi+1)]-_data[_index(xi+1,yi+2,zi-1)]+_data[_index(xi+1,yi,zi-1)]),
+			0.25*(_data[_index(xi,yi+1,zi+2)]-_data[_index(xi,yi-1,zi+2)]-_data[_index(xi,yi+1,zi)]+_data[_index(xi,yi-1,zi)]),
+			0.25*(_data[_index(xi+1,yi+1,zi+2)]-_data[_index(xi+1,yi-1,zi+2)]-_data[_index(xi+1,yi+1,zi)]+_data[_index(xi+1,yi-1,zi)]),
+			0.25*(_data[_index(xi,yi+2,zi+2)]-_data[_index(xi,yi,zi+2)]-_data[_index(xi,yi+2,zi)]+_data[_index(xi,yi,zi)]),
+			0.25*(_data[_index(xi+1,yi+2,zi+2)]-_data[_index(xi+1,yi,zi+2)]-_data[_index(xi+1,yi+2,zi)]+_data[_index(xi+1,yi,zi)])
+		};
+		double d3fdxdydzval[8] = {
+		    0.125f*(_data[_index(xi+1,yi+1,zi+1)]-_data[_index(xi-1,yi+1,zi+1)]-_data[_index(xi+1,yi-1,zi+1)]+_data[_index(xi-1,yi-1,zi+1)]-_data[_index(xi+1,yi+1,zi-1)]+_data[_index(xi-1,yi+1,zi-1)]+_data[_index(xi+1,yi-1,zi-1)]-_data[_index(xi-1,yi-1,zi-1)]),
+			0.125f*(_data[_index(xi+2,yi+1,zi+1)]-_data[_index(xi,yi+1,zi+1)]-_data[_index(xi+2,yi-1,zi+1)]+_data[_index(xi,yi-1,zi+1)]-_data[_index(xi+2,yi+1,zi-1)]+_data[_index(xi,yi+1,zi-1)]+_data[_index(xi+2,yi-1,zi-1)]-_data[_index(xi,yi-1,zi-1)]),
+			0.125f*(_data[_index(xi+1,yi+2,zi+1)]-_data[_index(xi-1,yi+2,zi+1)]-_data[_index(xi+1,yi,zi+1)]+_data[_index(xi-1,yi,zi+1)]-_data[_index(xi+1,yi+2,zi-1)]+_data[_index(xi-1,yi+2,zi-1)]+_data[_index(xi+1,yi,zi-1)]-_data[_index(xi-1,yi,zi-1)]),
+			0.125f*(_data[_index(xi+2,yi+2,zi+1)]-_data[_index(xi,yi+2,zi+1)]-_data[_index(xi+2,yi,zi+1)]+_data[_index(xi,yi,zi+1)]-_data[_index(xi+2,yi+2,zi-1)]+_data[_index(xi,yi+2,zi-1)]+_data[_index(xi+2,yi,zi-1)]-_data[_index(xi,yi,zi-1)]),
+			0.125f*(_data[_index(xi+1,yi+1,zi+2)]-_data[_index(xi-1,yi+1,zi+2)]-_data[_index(xi+1,yi-1,zi+2)]+_data[_index(xi-1,yi-1,zi+2)]-_data[_index(xi+1,yi+1,zi)]+_data[_index(xi-1,yi+1,zi)]+_data[_index(xi+1,yi-1,zi)]-_data[_index(xi-1,yi-1,zi)]),
+			0.125f*(_data[_index(xi+2,yi+1,zi+2)]-_data[_index(xi,yi+1,zi+2)]-_data[_index(xi+2,yi-1,zi+2)]+_data[_index(xi,yi-1,zi+2)]-_data[_index(xi+2,yi+1,zi)]+_data[_index(xi,yi+1,zi)]+_data[_index(xi+2,yi-1,zi)]-_data[_index(xi,yi-1,zi)]),
+			0.125f*(_data[_index(xi+1,yi+2,zi+2)]-_data[_index(xi-1,yi+2,zi+2)]-_data[_index(xi+1,yi,zi+2)]+_data[_index(xi-1,yi,zi+2)]-_data[_index(xi+1,yi+2,zi)]+_data[_index(xi-1,yi+2,zi)]+_data[_index(xi+1,yi,zi)]-_data[_index(xi-1,yi,zi)]),
+			0.125f*(_data[_index(xi+2,yi+2,zi+2)]-_data[_index(xi,yi+2,zi+2)]-_data[_index(xi+2,yi,zi+2)]+_data[_index(xi,yi,zi+2)]-_data[_index(xi+2,yi+2,zi)]+_data[_index(xi,yi+2,zi)]+_data[_index(xi+2,yi,zi)]-_data[_index(xi,yi,zi)])
+		};
+        // Remember this voxel for next time.
+        _i1 = xi;
+        _i2 = yi;
+        _i3 = zi;
+        _initialized = true;
+    }
+    double result(0);
+    for (int i=0;i<4;++i) {
+		for (int j=0;j<4;++j) {
+			for (int k=0;k<4;++k) {
+				result += _coefs[i+4*j+16*k]*pow(x,i)*pow(y,j)*pow(z,k);
+			}
+		}
+	}
+    return result;
+}
--- likely/TriCubicInterpolator.h
+++ likely/TriCubicInterpolator.h
@@ -10,12 +10,32 @@ namespace likely {
 	// Performs tri-cubic interpolation within a 3D periodic grid.
 	public:
         typedef boost::shared_array<double> DataCube;
+        // Initializes an interpolator using the specified datacube of length n1*n2*n3 where
+        // data is ordered first along the n1 axis [0,0,0], [1,0,0], ..., [n1-1,0,0], [0,1,0], ...
+        // If n2 and n3 are both omitted, then n1=n2=n3 is assumed. Data is assumed to be
+        // equally spaced and periodic along each axis.
 		TriCubicInterpolator(DataCube data, int n1, int n2 = 0, int n3 = 0);
 		virtual ~TriCubicInterpolator();
+        // Returns the interpolated data value for the specified x,y,z point in the unit cube.
+        double operator()(double x, double y, double z) const;
 	private:
+	    // Returns the unrolled 1D index corresponding to [i1,i2,i3] after mapping to each ik into [0,nk).
+	    // Assumes that i1 increases fastest in the 1D array.
+        int _index(int i1, int i2, int i3) const;
         DataCube _data;
         int _n1, _n2, _n3;
+        mutable int _i1, _i2, _i3;
+        mutable double _coefs[64];
+        mutable bool _initialized;
 	}; // TriCubicInterpolator
+	
+	inline int TriCubicInterpolator::_index(int i1, int i2, int i3) const {
+        if((i1 %= _n1) < 0) i1 += _n1;
+        if((i2 %= _n2) < 0) i2 += _n2;
+        if((i3 %= _n3) < 0) i3 += _n3;
+        return i1 + _n1*(i2 + _n2*i3);
+	}
+
 } // likely
 
 #endif // LIKELY_TRI_CUBIC_INTERPOLATOR
