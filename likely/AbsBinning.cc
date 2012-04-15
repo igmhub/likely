511d08d0eb6ef3843d065b0341ad5b51d25b0146
David Kirkby <dkirkby@uci.edu>
2012-04-14 21:41:57 -0700

    Declare and implement abstract binning base class.

--- likely/AbsBinning.cc
+++ likely/AbsBinning.cc
@@ -2,8 +2,21 @@
 
 #include "likely/AbsBinning.h"
 
+#include <iostream>
+
 namespace local = likely;
 
 local::AbsBinning::AbsBinning() { }
 
 local::AbsBinning::~AbsBinning() { }
+
+double local::AbsBinning::getBinCenter(int index) const {
+    return getBinLowEdge(index) + 0.5*getBinSize(index);
+}
+
+void local::AbsBinning::dump(std::ostream &os) const {
+    int nbins(getNBins());
+    os << nbins;
+    for(int bin = 0; bin <= nbins; ++bin) os << ' ' << getBinLowEdge(bin);
+    os << std::endl;
+}
--- likely/AbsBinning.h
+++ likely/AbsBinning.h
@@ -3,11 +3,25 @@
 #ifndef LIKELY_ABS_BINNING
 #define LIKELY_ABS_BINNING
 
+#include <iosfwd>
+
 namespace likely {
 	class AbsBinning {
 	public:
 		AbsBinning();
 		virtual ~AbsBinning();
+        // Returns the bin index [0,nBins-1] or else -1.
+        virtual int getBinIndex(double value) const = 0;
+        // Returns the total number of bins.
+        virtual int getNBins() const = 0;
+        // Returns the full width of the specified bin.
+        virtual double getBinSize(int index) const = 0;
+        // Returns the lower bound of the specified bin. Use index=nbins for the upper bound of the last bin.
+        virtual double getBinLowEdge(int index) const = 0;
+        // Returns the midpoint value of the specified bin.
+        virtual double getBinCenter(int index) const;
+        // Dumps this binning to the specified output stream in a standard format.
+        void dump(std::ostream &os) const;
 	private:
 	}; // AbsBinning
 } // likely
