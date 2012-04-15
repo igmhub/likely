75ceba6c5a638f25f26474464aac8fb3f9c387cc
David Kirkby <dkirkby@uci.edu>
2012-04-14 21:34:29 -0700

    Add skeleton new class AbsBinning.

---
+++ likely/AbsBinning.cc
@@ -0,0 +1,9 @@
+// Created 14-Apr-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "likely/AbsBinning.h"
+
+namespace local = likely;
+
+local::AbsBinning::AbsBinning() { }
+
+local::AbsBinning::~AbsBinning() { }
---
+++ likely/AbsBinning.h
@@ -0,0 +1,15 @@
+// Created 14-Apr-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#ifndef LIKELY_ABS_BINNING
+#define LIKELY_ABS_BINNING
+
+namespace likely {
+	class AbsBinning {
+	public:
+		AbsBinning();
+		virtual ~AbsBinning();
+	private:
+	}; // AbsBinning
+} // likely
+
+#endif // LIKELY_ABS_BINNING
