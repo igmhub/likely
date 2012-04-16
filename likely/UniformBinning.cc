5ce884875f0ae5d5941bfacde650ecb5d1772871
David Kirkby <dkirkby@uci.edu>
2012-04-16 13:31:26 -0700

    Implement minimal UniformBinning.

--- likely/UniformBinning.cc
+++ likely/UniformBinning.cc
@@ -1,9 +1,34 @@
 // Created 14-Apr-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "likely/UniformBinning.h"
+#include "likely/BinningError.h"
 
 namespace local = likely;
 
-local::UniformBinning::UniformBinning() { }
+local::UniformBinning::UniformBinning(double minValue, double maxValue, int nBins)
+: _minValue(minValue), _maxValue(maxValue), _nBins(nBins)
+{
+    if(maxValue <= minValue) {
+        throw BinningError("UniformBinning: expected minValue < maxValue.");
+    }
+    if(nBins <= 0) {
+        throw BinningError("UniformBinning: expected nBins > 0.");
+    }
+    _binWidth = (maxValue - minValue)/nBins;
+}
 
 local::UniformBinning::~UniformBinning() { }
+
+int local::UniformBinning::getNBins() const {
+    return _nBins;
+}
+
+double local::UniformBinning::getBinLowEdge(int index) const {
+    isValidBinIndex(index,"getBinLowEdge: invalid bin index %d.");
+    return _minValue + index*_binWidth;
+}
+
+double local::UniformBinning::getBinHighEdge(int index) const {
+    isValidBinIndex(index,"getBinHighEdge: invalid bin index %d.");
+    return _minValue + (index+1)*_binWidth;
+}
--- likely/UniformBinning.h
+++ likely/UniformBinning.h
@@ -3,12 +3,34 @@
 #ifndef LIKELY_UNIFORM_BINNING
 #define LIKELY_UNIFORM_BINNING
 
+#include "likely/AbsBinning.h"
+
 namespace likely {
-	class UniformBinning {
+	class UniformBinning : public AbsBinning {
+	// Represents a uniform 1D binning of a finite interval.
 	public:
-		UniformBinning();
+	    // Creates a new uniform binning for the interval [minValue,maxValue] using the specified
+	    // number of bins. Throws a BinningError of maxValue <= minValue or nBins <= 0.
+		UniformBinning(double minValue, double maxValue, int nBins);
 		virtual ~UniformBinning();
+        // Returns the total number of bins.
+        virtual int getNBins() const;
+        // Returns the lower bound of the specified bin. Throws a BinningError if index is out of range.
+        virtual double getBinLowEdge(int index) const;
+        // Returns the upper bound of the specified bin. Throws a BinningError if index is out of range.
+        virtual double getBinHighEdge(int index) const;
+        /*
+        // Returns the full width (hi-lo) of the specified bin, which might be zero if this bin represents
+        // a point sample rather than an integral over some interval. Throws a BinningError if index
+        // is out of range.
+        virtual double getBinWidth(int index) const;
+        // Returns the midpoint value (lo+hi)/2 of the specified bin. Throws a BinningError if index is
+        // out of range.
+        virtual double getBinCenter(int index) const;
+        */
 	private:
+        double _minValue, _maxValue, _binWidth;
+        int _nBins;
 	}; // UniformBinning
 } // likely
 
