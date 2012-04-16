b7a00f98ae04d096dcbce30f1ca06069cd95518c
David Kirkby <dkirkby@uci.edu>
2012-04-16 15:10:28 -0700

    Implement UniformSampling class.

--- likely/UniformSampling.cc
+++ likely/UniformSampling.cc
@@ -1,9 +1,45 @@
 // Created 16-Apr-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "likely/UniformSampling.h"
+#include "likely/BinningError.h"
 
 namespace local = likely;
 
-local::UniformSampling::UniformSampling() { }
+local::UniformSampling::UniformSampling(double minValue, double maxValue, int nSamples)
+: _minValue(minValue), _maxValue(maxValue), _nSamples(nSamples)
+{
+    if(maxValue < minValue) {
+        throw BinningError("UniformSampling: expected minValue <= maxValue.");
+    }
+    if(nSamples < 1) {
+        throw BinningError("UniformSampling: expected nSamples > 1.");
+    }
+    if(nSamples == 1 && maxValue != minValue) {
+        throw BinningError("UniformSampling: must have minValue==maxValue when nSamples==1.");
+    }
+    _sampleSpacing = (1==nSamples) ? 0 : (maxValue - minValue)/(nSamples-1.0);
+}
 
 local::UniformSampling::~UniformSampling() { }
+
+int local::UniformSampling::getNBins() const {
+    return _nSamples;
+}
+
+double local::UniformSampling::getBinLowEdge(int index) const {
+    return getBinCenter(index);
+}
+
+double local::UniformSampling::getBinHighEdge(int index) const {
+    return getBinCenter(index);
+}
+
+double local::UniformSampling::getBinWidth(int index) const {
+    isValidBinIndex(index,"getBinWidth: invalid bin index %d.");
+    return 0;
+}
+
+double local::UniformSampling::getBinCenter(int index) const {
+    isValidBinIndex(index,"getBinCenter: invalid bin index %d.");
+    return _minValue + index*_sampleSpacing;
+}
--- likely/UniformSampling.h
+++ likely/UniformSampling.h
@@ -3,12 +3,33 @@
 #ifndef LIKELY_UNIFORM_SAMPLING
 #define LIKELY_UNIFORM_SAMPLING
 
+#include "likely/AbsBinning.h"
+
 namespace likely {
-	class UniformSampling {
+	// Represents a uniform 1D sampling of a finite interval. A sampling is a binning with zero-width
+	// bins, and represents point samples of an interval rather than bin integrals.
+	class UniformSampling : public AbsBinning {
 	public:
-		UniformSampling();
+	    // Creates a new uniform sampling for the interval [minValue,maxValue] using the specified
+	    // number of samples. Throws a BinningError unless maxValue > minValue and nSamples > 1,
+	    // or else maxValue==minValue and nSamples==1.
+		UniformSampling(double minValue, double maxValue, int nSamples);
 		virtual ~UniformSampling();
+        // Returns the total number of bins, which is equal to the number of samples.
+        virtual int getNBins() const;
+        // Returns the lower bound of the specified bin. Throws a BinningError if index is out of range.
+        virtual double getBinLowEdge(int index) const;
+        // Returns the upper bound of the specified bin. Throws a BinningError if index is out of range.
+        virtual double getBinHighEdge(int index) const;
+        // Returns the full width (hi-lo) of the specified bin, which is zero for a sampling.
+        // Throws a BinningError if index is out of range.
+        virtual double getBinWidth(int index) const;
+        // Returns the midpoint value (lo+hi)/2 of the specified bin. Throws a BinningError if index is
+        // out of range.
+        virtual double getBinCenter(int index) const;
 	private:
+        double _minValue, _maxValue, _sampleSpacing;
+        int _nSamples;
 	}; // UniformSampling
 } // likely
 
