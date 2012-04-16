65b22fd7736fd0bb85f5472f72b3b6a256a1bc6b
David Kirkby <dkirkby@uci.edu>
2012-04-16 14:48:49 -0700

    Add subclass NonUniformBinning.

---
+++ likely/NonUniformBinning.cc
@@ -0,0 +1,48 @@
+// Created 16-Apr-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "likely/NonUniformBinning.h"
+#include "likely/BinningError.h"
+
+namespace local = likely;
+
+local::NonUniformBinning::NonUniformBinning(std::vector<double> const &binEdges)
+: _binEdges(binEdges)
+{
+    // Check that we have some bin edges.
+    int nBins = _binEdges.size();
+    if(nBins < 2) {
+        throw BinningError("NonUniformBinning: need at least 2 bin edges.");
+    }
+    // Check that bin edges are increasing.
+    for(int index = 1; index < nBins; ++index) {
+        if(_binEdges[index-1] > _binEdges[index]) {
+            throw BinningError("NonUniformBinning: bin edges are not in increasing order.");
+        }
+    }
+}
+
+local::NonUniformBinning::~NonUniformBinning() { }
+
+int local::NonUniformBinning::getNBins() const {
+    return _binEdges.size()-1;
+}
+
+double local::NonUniformBinning::getBinLowEdge(int index) const {
+    isValidBinIndex(index,"getBinLowEdge: invalid bin index %d.");
+    return _binEdges[index];
+}
+
+double local::NonUniformBinning::getBinHighEdge(int index) const {
+    isValidBinIndex(index,"getBinHighEdge: invalid bin index %d.");
+    return _binEdges[index+1];
+}
+
+double local::NonUniformBinning::getBinWidth(int index) const {
+    isValidBinIndex(index,"getBinWidth: invalid bin index %d.");
+    return _binEdges[index+1] - _binEdges[index];
+}
+
+double local::NonUniformBinning::getBinCenter(int index) const {
+    isValidBinIndex(index,"getBinCenter: invalid bin index %d.");
+    return 0.5*(_binEdges[index] + _binEdges[index+1]);
+}
