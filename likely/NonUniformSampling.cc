32c1b997d8a9e9d22fb2cbc07cae51bcd1d59c8d
David Kirkby <dkirkby@uci.edu>
2012-04-16 16:27:17 -0700

    Implement NonUniformSampling class.

--- likely/NonUniformSampling.cc
+++ likely/NonUniformSampling.cc
@@ -1,9 +1,46 @@
 // Created 16-Apr-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "likely/NonUniformSampling.h"
+#include "likely/BinningError.h"
 
 namespace local = likely;
 
-local::NonUniformSampling::NonUniformSampling() { }
+local::NonUniformSampling::NonUniformSampling(std::vector<double> const &samplePoints)
+: _samplePoints(samplePoints)
+{
+    // Check that we have some samples.
+    int nSamples = _samplePoints.size();
+    if(nSamples < 1) {
+        throw BinningError("NonUniformSampling: need at least 1 sample point.");
+    }
+    // Check that sample points are increasing.
+    for(int index = 1; index < nSamples; ++index) {
+        if(_samplePoints[index-1] > _samplePoints[index]) {
+            throw BinningError("NonUniformSampling: sample points are not in increasing order.");
+        }
+    }
+}
 
 local::NonUniformSampling::~NonUniformSampling() { }
+
+int local::NonUniformSampling::getNBins() const {
+    return _samplePoints.size();
+}
+
+double local::NonUniformSampling::getBinLowEdge(int index) const {
+    return getBinCenter(index);
+}
+
+double local::NonUniformSampling::getBinHighEdge(int index) const {
+    return getBinCenter(index);
+}
+
+double local::NonUniformSampling::getBinWidth(int index) const {
+    isValidBinIndex(index,"getBinWidth: invalid bin index %d.");
+    return 0;
+}
+
+double local::NonUniformSampling::getBinCenter(int index) const {
+    isValidBinIndex(index,"getBinCenter: invalid bin index %d.");
+    return _samplePoints[index];
+}
--- likely/NonUniformSampling.h
+++ likely/NonUniformSampling.h
@@ -3,12 +3,34 @@
 #ifndef LIKELY_NON_UNIFORM_SAMPLING
 #define LIKELY_NON_UNIFORM_SAMPLING
 
+#include "likely/AbsBinning.h"
+
+#include <vector>
+
 namespace likely {
-	class NonUniformSampling {
+	// Represents a non-uniform 1D sampling of a finite interval. A sampling is a binning with zero-width
+	// bins, and represents point samples of an interval rather than bin integrals.
+	class NonUniformSampling : public AbsBinning {
 	public:
-		NonUniformSampling();
+	    // Creates a new sampling using the specified list of sample points, which must be in increasing
+	    // order. Throws a BinningError unless maxValue >= minValue.
+		explicit NonUniformSampling(std::vector<double> const &samplePoints);
 		virtual ~NonUniformSampling();
+        // Returns the total number of bins.
+        virtual int getNBins() const;
+        // Returns the lower bound of the specified bin. Throws a BinningError if index is out of range.
+        virtual double getBinLowEdge(int index) const;
+        // Returns the upper bound of the specified bin. Throws a BinningError if index is out of range.
+        virtual double getBinHighEdge(int index) const;
+        // Returns the full width (hi-lo) of the specified bin, which might be zero if this bin represents
+        // a point sample rather than an integral over some interval. Throws a BinningError if index
+        // is out of range.
+        virtual double getBinWidth(int index) const;
+        // Returns the midpoint value (lo+hi)/2 of the specified bin. Throws a BinningError if index is
+        // out of range.
+        virtual double getBinCenter(int index) const;
 	private:
+        std::vector<double> _samplePoints;
 	}; // NonUniformSampling
 } // likely
 
