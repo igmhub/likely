f91c224c5d31583d6ebc2d3b71a3e5f16ae44557
David Kirkby <dkirkby@uci.edu>
2012-04-17 15:11:14 -0700

    Define and implement API for covariance matrix size and compression.

--- likely/AbsCovarianceMatrix.h
+++ likely/AbsCovarianceMatrix.h
@@ -4,12 +4,42 @@
 #define LIKELY_ABS_COVARIANCE_MATRIX
 
 namespace likely {
+    // Represents an abstract interface to a covariance matrix.
 	class AbsCovarianceMatrix {
 	public:
-		AbsCovarianceMatrix();
 		virtual ~AbsCovarianceMatrix();
+		// Returns the fixed size of this covariance matrix.
+        int getSize() const;
+        // Requests that this covariance matrix be compressed to reduce its memory usage,
+        // if possible. Returns immediately if we are already compressed.
+        void compress();
+        // Undoes any compression. Returns immediately if we are already uncompressed.
+        void uncompress();
+        // Returns true if this covariance matrix is currently compressed.
+        bool isCompressed() const;
+	protected:
+	    // Creates a new size-by-size covariance matrix. Throws a RuntimeError if size <= 0.
+		explicit AbsCovarianceMatrix(int size);
+		// Does the actual work of compressing a matrix, if possible.
+        virtual void doCompress() = 0;
+        // Does the actual work of undoing any compression performed by doCompress().
+        virtual void doUncompress() = 0;
 	private:
+        int _size;
+        bool _compressed;
 	}; // AbsCovarianceMatrix
+	
+    inline int AbsCovarianceMatrix::getSize() const { return _size; }
+    
+    inline void AbsCovarianceMatrix::compress() {
+        if(!_compressed) { doCompress(); _compressed = true; }
+    }
+    inline void AbsCovarianceMatrix::uncompress() {
+        if(_compressed) { doUncompress(); _compressed = false; }
+    }
+    
+    inline bool AbsCovarianceMatrix::isCompressed() const { return _compressed; }
+
 } // likely
 
 #endif // LIKELY_ABS_COVARIANCE_MATRIX
