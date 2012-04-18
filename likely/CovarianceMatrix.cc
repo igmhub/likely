fb4ce30f0ad959673c9267c74b94a150e970bb3a
David Kirkby <dkirkby@uci.edu>
2012-04-18 12:33:55 -0700

    Start implementing low-level routines of CovarianceMatrix.

--- likely/CovarianceMatrix.cc
+++ likely/CovarianceMatrix.cc
@@ -3,6 +3,23 @@
 #include "likely/CovarianceMatrix.h"
 #include "likely/RuntimeError.h"
 
+#include <cassert>
+
+// Declare bindings to BLAS,LAPACK routines we need
+extern "C" {
+    // http://www.netlib.org/lapack/double/dpptrf.f
+    void dpptrf_(char const *uplo, int const *n, double *ap, int *info);
+    // http://www.netlib.org/lapack/double/dpptri.f
+    void dpptri_(char const *uplo, int const *n, double *ap, int *info);
+    // http://netlib.org/blas/dspmv.f
+    void dspmv_(char const *uplo, int const *n, double const *alpha, double const *ap,
+        double const *x, int const *incx, double const *beta, double *y, int const *incy);
+    // http://www.netlib.org/blas/dsymm.f
+    void dsymm_(char const *side, char const *uplo, int const *m, int const *n,
+        double const *alpha, double const *a, int const *lda, double const *b,
+        int const *ldb, double const *beta, double *c, int const *ldc);
+}
+
 namespace local = likely;
 
 local::CovarianceMatrix::CovarianceMatrix(int size)
@@ -11,6 +28,85 @@ local::CovarianceMatrix::CovarianceMatrix(int size)
     if(size <= 0) {
         throw RuntimeError("CovarianceMatrix: expected size > 0.");
     }
+    _ncov = (_size*(_size+1))/2;
 }
 
 local::CovarianceMatrix::~CovarianceMatrix() { }
+
+void local::CovarianceMatrix::compress() {
+    if(_compressed) return;
+}
+void local::CovarianceMatrix::uncompress() {
+    if(!_compressed) return;
+}
+
+int local::symmetricMatrixIndex(int row, int col, int size) {
+    if(row < 0 || col < 0 || row >= size || col >= size) {
+        throw RuntimeError("symmetricMatrixIndex: row or col out of range.");
+    }
+    // Ensure that row <= col
+    if(row > col) std::swap(row,col);
+    return row+(col*(col+1))/2;
+}
+
+void local::choleskyDecompose(std::vector<double> &matrix) {
+    static char uplo('U');
+    static int info(0);
+    int n(matrix.size());
+    dpptrf_(&uplo,&n,&matrix[0],&info);
+    if(0 != info) {
+        info = 0;
+        throw RuntimeError("choleskyDecomposition: matrix is not positive definite.");
+    }    
+}
+
+void local::invertCholesky(std::vector<double> &matrix) {
+    static char uplo('U');
+    static int info(0);
+    int n(matrix.size());
+    dpptri_(&uplo,&n,&matrix[0],&info);
+    if(0 != info) {
+        info = 0;
+        throw RuntimeError("invertCholesky: symmetric matrix inversion failed.");
+    }    
+} 
+
+void local::CovarianceMatrix::_changesCov() {
+    // Do we have a valid matrix to change?
+    if(_cov.empty()) {
+        // Have we allocated anything yet?
+        if(_icov.empty()) {
+            // Allocate a covariance matrix initialized to zero.
+            std::vector<double>(_ncov,0).swap(_cov);
+        }
+        else {
+            // Try to invert the existing inverse covariance in place. This will throw a
+            // RuntimeError in case the existing inverse covariance is only partially filled in.
+            choleskyDecompose(_icov);
+            invertCholesky(_icov);
+            // Remove the existing inverse covariance (by swapping with _cov), since it will
+            // become invalid after we update the the covariance.
+            _cov.swap(_icov);
+            // Remove any existing Cholesky decomposition since it will become invalid.
+            if(!_cholesky.empty()) std::vector<double>().swap(_cholesky);
+        }
+    }
+    assert(!_cov.empty());
+    assert(_icov.empty());
+    assert(_cholesky.empty());
+}
+
+void local::CovarianceMatrix::setCovariance(int row, int col, double value) {
+    // Calculate the index corresponding to (row,col). This will throw a RuntimeError
+    // in case of an invalid address, before we actually change anything.
+    int index(symmetricMatrixIndex(row,col,_size));
+    // Prepare to change the covariance matrix, which might throw a RuntimeError
+    // if icov elements have already been set, but icov is not invertible.
+    _changesCov();
+    // Finally, set the new value here.
+    _cov[index] = value;
+}
+
+void local::CovarianceMatrix::setInverseCovariance(int row, int col, double value) {
+    
+}
--- likely/CovarianceMatrix.h
+++ likely/CovarianceMatrix.h
@@ -3,13 +3,24 @@
 #ifndef LIKELY_COVARIANCE_MATRIX
 #define LIKELY_COVARIANCE_MATRIX
 
+#include <vector>
+
 namespace likely {
     // Represents an abstract interface to a covariance matrix.
 	class CovarianceMatrix {
 	public:
+	    // Creates a new size-by-size covariance matrix with all elements initialized to zero.
+	    // Throws a RuntimeError if size <= 0.
+		explicit CovarianceMatrix(int size);
 		virtual ~CovarianceMatrix();
 		// Returns the fixed size of this covariance matrix.
         int getSize() const;
+        // Sets the specified (inverse) covariance matrix element or throws a RuntimeError
+        // if this is not possible (when a required matrix inversion fails). Row and column
+        // indices should be in the range [0,size-1]. Setting any element with row != col will
+        // also set the symmetric element in the matrix.
+        void setCovariance(int row, int col, double value);
+        void setInverseCovariance(int row, int col, double value);
         // Requests that this covariance matrix be compressed to reduce its memory usage,
         // if possible. Returns immediately if we are already compressed.
         void compress();
@@ -17,28 +28,30 @@ namespace likely {
         void uncompress();
         // Returns true if this covariance matrix is currently compressed.
         bool isCompressed() const;
-	protected:
-	    // Creates a new size-by-size covariance matrix. Throws a RuntimeError if size <= 0.
-		explicit CovarianceMatrix(int size);
-		// Does the actual work of compressing a matrix, if possible.
-        virtual void doCompress() = 0;
-        // Does the actual work of undoing any compression performed by doCompress().
-        virtual void doUncompress() = 0;
 	private:
-        int _size;
+        // Prepares to change at least one element of the _cov array.
+        void _changesCov();
+        int _size, _ncov;
         bool _compressed;
+        std::vector<double> _cov, _icov, _cholesky;
 	}; // CovarianceMatrix
 	
     inline int CovarianceMatrix::getSize() const { return _size; }
     
-    inline void CovarianceMatrix::compress() {
-        if(!_compressed) { doCompress(); _compressed = true; }
-    }
-    inline void CovarianceMatrix::uncompress() {
-        if(_compressed) { doUncompress(); _compressed = false; }
-    }
-    
     inline bool CovarianceMatrix::isCompressed() const { return _compressed; }
+
+    // Returns the array offset index for the BLAS packed symmetric matrix format
+    // described at http://www.netlib.org/lapack/lug/node123.html or throws a
+    // RuntimeError for invalid row or col inputs.
+    int symmetricMatrixIndex(int row, int col, int size);
+    // Performs a Cholesky decomposition in place of a symmetric positive definite matrix
+    // or throws a RuntimeError if the matrix is not positive definite. The input matrix
+    // is assumed to be in the BLAS packed format implied by packedMatrixIndex(row,col).
+    static void choleskyDecompose(std::vector<double> &matrix);
+    // Inverts a symmetric positive definite matrix in place, or throws a RuntimeError.
+    // The input matrix should already be Cholesky decomposed and in the BLAS packed format
+    // implied by packedMatrixIndex(row,col), e.g. by first calling _choleskyDecompose(matrix).
+    static void invertCholesky(std::vector<double> &matrix);
 
 } // likely
 
