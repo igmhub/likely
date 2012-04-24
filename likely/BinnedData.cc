cbe0d61cc83d7ee1d4f8c9ace7756e373b65d0cf
David Kirkby <dkirkby@uci.edu>
2012-04-24 08:39:46 -0700

    Implement constructors and getDimension().

--- likely/BinnedData.cc
+++ likely/BinnedData.cc
@@ -1,9 +1,43 @@
 // Created 24-Apr-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "likely/BinnedData.h"
+#include "likely/RuntimeError.h"
 
 namespace local = likely;
 
-local::BinnedData::BinnedData() { }
+local::BinnedData::BinnedData(std::vector<AbsBinningCPtr> axes)
+: _axisBinning(axes)
+{
+    if(0 == axes.size()) {
+        throw RuntimeError("BinnedData: no axes provided.");
+    }
+}
+
+local::BinnedData::BinnedData(AbsBinningCPtr axis1)
+{
+    if(!axis1) {
+        throw RuntimeError("BinnedData: missing axis1.");
+    }
+    _axisBinning.push_back(axis1);
+}
+
+local::BinnedData::BinnedData(AbsBinningCPtr axis1, AbsBinningCPtr axis2)
+{
+    if(!axis1 || !axis2) {
+        throw RuntimeError("BinnedData: missing axis data.");
+    }
+    _axisBinning.push_back(axis1);    
+    _axisBinning.push_back(axis2);
+}
+
+local::BinnedData::BinnedData(AbsBinningCPtr axis1, AbsBinningCPtr axis2, AbsBinningCPtr axis3)
+{
+    if(!axis1 || !axis2 || !axis3) {
+        throw RuntimeError("BinnedData: missing axis data.");
+    }
+    _axisBinning.push_back(axis1);
+    _axisBinning.push_back(axis2);
+    _axisBinning.push_back(axis3);
+}
 
 local::BinnedData::~BinnedData() { }
--- likely/BinnedData.h
+++ likely/BinnedData.h
@@ -3,13 +3,31 @@
 #ifndef LIKELY_BINNED_DATA
 #define LIKELY_BINNED_DATA
 
+#include "likely/types.h"
+
+#include <vector>
+
 namespace likely {
+    // Represents data that is binned independently along one or more axes.
 	class BinnedData {
 	public:
-		BinnedData();
+	    // Creates a new dataset with an arbitrary number of axes.
+        explicit BinnedData(std::vector<AbsBinningCPtr> axes);
+        // Creates a new dataset with a single axis.
+		explicit BinnedData(AbsBinningCPtr axis1);
+        // Creates a new dataset with two axes.
+        BinnedData(AbsBinningCPtr axis1, AbsBinningCPtr axis2);
+        // Creates a new dataset with three axes.
+        BinnedData(AbsBinningCPtr axis1, AbsBinningCPtr axis2, AbsBinningCPtr axis3);
 		virtual ~BinnedData();
+		// Returns the number of axes used to bin this data.
+        int getDimension() const;
 	private:
+        std::vector<AbsBinningCPtr> _axisBinning;
 	}; // BinnedData
+	
+    inline int BinnedData::getDimension() const { return _axisBinning.size(); }
+
 } // likely
 
 #endif // LIKELY_BINNED_DATA
