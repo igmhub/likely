d5fe64adfa141e2779197076ba134f139055dc82
David Kirkby <dkirkby@uci.edu>
2012-06-08 06:20:34 -0700

    Add skeleton FitModel class.

---
+++ likely/FitModel.cc
@@ -0,0 +1,9 @@
+// Created 08-Jun-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "likely/FitModel.h"
+
+namespace local = likely;
+
+local::FitModel::FitModel() { }
+
+local::FitModel::~FitModel() { }
---
+++ likely/FitModel.h
@@ -0,0 +1,15 @@
+// Created 08-Jun-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#ifndef LIKELY_FIT_MODEL
+#define LIKELY_FIT_MODEL
+
+namespace likely {
+	class FitModel {
+	public:
+		FitModel();
+		virtual ~FitModel();
+	private:
+	}; // FitModel
+} // likely
+
+#endif // LIKELY_FIT_MODEL
