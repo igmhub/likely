5c975ce749e52ad4f02cb88190ae20ffde61db59
David Kirkby <dkirkby@uci.edu>
2012-06-08 07:24:02 -0700

    Implement baofit::AbsCorrelationModel functionality.

--- likely/FitModel.cc
+++ likely/FitModel.cc
@@ -1,9 +1,36 @@
 // Created 08-Jun-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "likely/FitModel.h"
+#include "likely/RuntimeError.h"
+#include "likely/AbsEngine.h"
+
+#include <iostream>
 
 namespace local = likely;
 
-local::FitModel::FitModel() { }
+local::FitModel::FitModel(std::string const &name)
+: _name(name)
+{ }
 
 local::FitModel::~FitModel() { }
+
+void local::FitModel::defineParameter(std::string const &name, double value, double error) {
+    _parameters.push_back(FitParameter(name,value,error));
+}
+
+int local::FitModel::getNParameters(bool onlyFloating) const {
+    return onlyFloating ? countFloatingFitParameters(_parameters) : _parameters.size();
+}
+
+void  local::FitModel::printToStream(std::ostream &out, std::string const &formatSpec) const {
+    out << "Fit Model \"" << _name << "\" has initial parameters:" << std::endl;
+    printFitParametersToStream(_parameters,out,formatSpec);
+}
+
+void local::FitModel::configure(std::string const &script) {
+    modifyFitParameters(_parameters,script);
+}
+
+local::FunctionMinimumPtr local::FitModel::findMinimum(FunctionPtr fptr, std::string const &method) const {
+    return local::findMinimum(fptr, _parameters, method);
+}
--- likely/FitModel.h
+++ likely/FitModel.h
@@ -3,13 +3,42 @@
 #ifndef LIKELY_FIT_MODEL
 #define LIKELY_FIT_MODEL
 
+#include "likely/FitParameter.h"
+
+#include <string>
+#include <iosfwd>
+
 namespace likely {
 	class FitModel {
 	public:
-		FitModel();
+		FitModel(std::string const &name);
 		virtual ~FitModel();
+		// Returns a copy of our model name.
+        std::string getName() const;
+        // Returns the number of model parameters.
+        int getNParameters(bool onlyFloating = false) const;
+        // Prints a multi-line description of this object to the specified output stream.
+        virtual void printToStream(std::ostream &out, std::string const &formatSpec = "%12.6f") const;
+        // Configures our fit parameters using the specified script. See the documentation for
+        // modifyFitParameters in FitParameters.h for details.
+        virtual void configure(std::string const &script);
+        // Finds the minimum of the specified function with respect to our parameters,
+        // using the specified method. Use the configure method to change the initial
+        // parameter values and errors.
+        FunctionMinimumPtr findMinimum(FunctionPtr fptr, std::string const &method) const;
+    protected:
+        // Subclasses use this method to define their parameters. Parameters should generally
+        // be specified with a reasonable error > 0 since the configure() method provides a
+        // convenient way to fix a parameter before a fit. Throws a RuntimeError for an
+        // invalid parameter name or error < 0.
+        void defineParameter(std::string const &name, double value, double error);
 	private:
+        std::string _name;
+        FitParameters _parameters;
 	}; // FitModel
+	
+    inline std::string FitModel::getName() const { return _name; }
+	
 } // likely
 
 #endif // LIKELY_FIT_MODEL
