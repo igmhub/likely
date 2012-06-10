60bda3b27cb84e4bb2522b438f5176a4da4d58bb
Daniel Margala <dmargala@uci.edu>
2012-06-10 12:17:03 -0700

    being implementing BinnedData class unit tests

--- test/BinnedDataTest.cc
+++ test/BinnedDataTest.cc
@@ -7,10 +7,25 @@
 #include "likely/likely.h"
 namespace lk = likely;
 
-BOOST_AUTO_TEST_SUITE( BinnedData )
+struct BinnedDataFixture
+{
+    BinnedDataFixture() {
+    	std::vector<double> bins(4);
+    	bins[0] = 0; bins[1] = 0.25; bins[2] = 0.35; bins[3] = 1;    
+    	lk::AbsBinningCPtr
+        	axis1(new lk::UniformBinning(0.,1.,3)),
+        	axis2(new lk::UniformSampling(0.,1.,3)),
+        	axis3(new lk::NonUniformBinning(bins));
+        binnedData.reset(new lk::BinnedData(axis1, axis2, axis3));
+    }
+    ~BinnedDataFixture() { }   
+    boost::shared_ptr<const lk::BinnedData> binnedData;
+};
 
-BOOST_AUTO_TEST_CASE( placeHolderTest ) {
+BOOST_FIXTURE_TEST_SUITE( BinnedData, BinnedDataFixture )
+
+BOOST_AUTO_TEST_CASE( shouldThrowBinningErrorInConstructorWithInvalidParameters ) {
 	BOOST_CHECK_EQUAL(1, 1);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END() // BinnedData
