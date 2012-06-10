e66ff0a17024cd73cb26dc7e7a7ee93cbe882f74
David Kirkby <dkirkby@uci.edu>
2012-06-10 16:14:23 -0400

    Create skeleton new class to accumulate fit parameter value statistics.

---
+++ likely/FitParameterStatistics.cc
@@ -0,0 +1,9 @@
+// Created 10-Jun-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "likely/FitParameterStatistics.h"
+
+namespace local = likely;
+
+local::FitParameterStatistics::FitParameterStatistics() { }
+
+local::FitParameterStatistics::~FitParameterStatistics() { }
---
+++ likely/FitParameterStatistics.h
@@ -0,0 +1,15 @@
+// Created 10-Jun-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#ifndef LIKELY_FIT_PARAMETER_STATISTICS
+#define LIKELY_FIT_PARAMETER_STATISTICS
+
+namespace likely {
+	class FitParameterStatistics {
+	public:
+		FitParameterStatistics();
+		virtual ~FitParameterStatistics();
+	private:
+	}; // FitParameterStatistics
+} // likely
+
+#endif // LIKELY_FIT_PARAMETER_STATISTICS
