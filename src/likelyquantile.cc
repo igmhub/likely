27bd7a0b880c82b71318ddae89b25bd577790211
Daniel Margala <dmargala@uci.edu>
2012-08-27 16:03:25 -0700

    Implement ExactQuantileAccumulator unit tests. Modify likelyquantile to run test quantile tests several times to generate more statistics.

--- src/likelyquantile.cc
+++ src/likelyquantile.cc
@@ -19,30 +19,42 @@ int main(int argc, char **argv) {
 		lk::ExactQuantileAccumulator exact;
 		// Initialize random number generator
 	    lk::RandomPtr random(new lk::Random());
-        random->setSeed(42);
+        random->setSeed(100);
 	    // Accumulate samples
-	    double sample;
-	    for(int j = 0; j < 20; ++j) {
-	    	long maxSamples = pow(10,1+j*.2);
-		    for(int i = 0; i < maxSamples; ++i){
-		    	sample = random->getNormal();
-		    	median.accumulate(sample);
-		    	s1.accumulate(sample);
-		    	s2.accumulate(sample);
-		    	s3.accumulate(sample);
-		    	exact.accumulate(sample);
-		    }
-			std::cout << maxSamples
-				<< " " << 0-median.getQuantile() 
-				<< " " << 1-s1.getQuantile() 
-				<< " " << 2-s2.getQuantile() 
-				<< " " << 3-s3.getQuantile() 
-				<< std::endl;
-			std::cout << maxSamples
-				<< " " << 0-exact.getQuantile(.5) 
-				<< " " << 1-exact.getQuantile(0.841345) 
-				<< " " << 2-exact.getQuantile(0.97725) 
-				<< " " << 3-exact.getQuantile(0.99865) 
+	    double ndecades = 2.5;
+	    double spacing = .2;
+		int ntrials = 100;
+	    for(int j = 0; j < ndecades/spacing; ++j) {
+	    	long nsamples = pow(10,1+j*spacing);
+	    	// Prepare quantile result accumulators
+	    	lk::WeightedAccumulator approx0,approx1,approx2,approx3,exact0,exact1,exact2,exact3;
+	    	for(int k = 0; k < ntrials; ++k) {
+			    for(int i = 0; i < nsamples; ++i){
+			    	double sample(random->getNormal());
+			    	median.accumulate(sample);
+			    	s1.accumulate(sample);
+			    	s2.accumulate(sample);
+			    	s3.accumulate(sample);
+			    	exact.accumulate(sample);
+			    }
+			    approx0.accumulate(median.getQuantile());
+			    approx1.accumulate(s1.getQuantile());
+			    approx2.accumulate(s2.getQuantile());
+			    approx3.accumulate(s3.getQuantile());
+			    exact0.accumulate(exact.getQuantile(0.5));
+			    exact1.accumulate(exact.getQuantile(0.841345));
+			    exact2.accumulate(exact.getQuantile(0.97725));
+			    exact3.accumulate(exact.getQuantile(0.99865));
+			}
+			std::cout << nsamples
+				<< " " << approx0.mean() << " " << approx0.error()
+				<< " " << 1 - approx1.mean() << " " << approx1.error()
+				<< " " << 2 - approx2.mean() << " " << approx2.error()
+				<< " " << 3 - approx3.mean() << " " << approx3.error()
+				<< " " << exact0.mean() << " " << exact0.error()
+				<< " " << 1 - exact1.mean() << " " << exact1.error()
+				<< " " << 2 - exact2.mean() << " " << exact2.error()
+				<< " " << 3 - exact3.mean() << " " << exact3.error()
 				<< std::endl;
 		}
 	}
