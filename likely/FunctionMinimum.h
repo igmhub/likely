a980d6c105ca6d0010bee2f451d40dfa1d951fd6
Anze Slosar <anze@bnl.gov>
2012-09-03 11:53:43 -0400

    Fixed MCMC
    from something that made no sense to something that hopefully works.

--- likely/FunctionMinimum.h
+++ likely/FunctionMinimum.h
@@ -58,10 +58,9 @@ namespace likely {
         // originally have one. This method does not update the errors associated with our
         // parameters, but calling it before updateParameterValues() will have this effect.
         void updateCovariance(CovarianceMatrixCPtr covariance);
-        // Sets parameter values that are randomly sampled from this minimum and
-        // returns the -log(weight) associated with the chosen parameters. Parameters that
-        // are not floating will be included in the result, but not varied.
-        double setRandomParameters(Parameters &params) const;
+        // Makes a proposal from fromParams to toParams by adding a vector sampled from  
+        // the covariance matrix to the fromParams
+        void setRandomParameters(const Parameters &fromParams, Parameters &toParams) const;
         // Sets the number of times the function and its gradient have been evaluated to
         // obtain this estimate of the minimum.
         void setCounts(long nEvalCount, long nGradCount);
