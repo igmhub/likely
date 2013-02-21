45ce0a581904055eadde19b160f03ef4c63108e5
David Kirkby <dkirkby@uci.edu>
2013-02-21 12:07:04 +0100

    Add createBinning function that parses a specification string and creates a corresponding binning object

--- likely/AbsBinning.cc
+++ likely/AbsBinning.cc
@@ -40,3 +40,45 @@ bool local::AbsBinning::isValidBinIndex(int index, std::string const &errorForma
         return false;
     }
 }
+
+#include "likely/UniformSampling.h"
+#include "likely/NonUniformSampling.h"
+
+#include "boost/spirit/include/qi.hpp"
+#include "boost/spirit/include/phoenix_core.hpp"
+#include "boost/spirit/include/phoenix_operator.hpp"
+#include "boost/spirit/include/phoenix_stl.hpp"
+
+namespace qi = boost::spirit::qi;
+namespace ascii = boost::spirit::ascii;
+namespace phoenix = boost::phoenix;
+
+local::AbsBinningCPtr local::createBinning(std::string const &binningSpec) {
+    // import boost spirit parser symbols
+    using qi::double_;
+    using qi::_1;
+    using qi::lit;
+    using phoenix::ref;
+    using phoenix::push_back;
+    
+    std::vector<double> centers;
+
+    // Parse the points string into a vector of doubles.
+    std::string::const_iterator iter = binningSpec.begin();
+    bool ok = qi::phrase_parse(iter,binningSpec.end(),
+        (
+            double_[push_back(ref(centers),_1)] % ','
+        ),
+        ascii::space);
+    if(!ok || iter != binningSpec.end()) {
+        throw RuntimeError("createBinning: badly formatted specification string.");
+    }
+    AbsBinningCPtr binning;
+    if(centers.size() > 2) {
+        binning.reset(new NonUniformSampling(centers));
+    }
+    else {
+        binning.reset(new UniformSampling(centers.front(),centers.back(),centers.size()));
+    }
+    return binning;
+}
