6c3dd3f385f29e05888138e693ba32e1d26cc17c
David Kirkby <dkirkby@uci.edu>
2013-05-10 09:15:02 +0200

    Copy stuff from BinnedData to BinnedGrid (no changes to BinnedData yet)

--- likely/BinnedGrid.cc
+++ likely/BinnedGrid.cc
@@ -1,9 +1,98 @@
 // Created 10-May-2013 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "likely/BinnedGrid.h"
+#include "likely/AbsBinning.h"
+#include "likely/RuntimeError.h"
+
+#include "boost/foreach.hpp"
+#include "boost/lexical_cast.hpp"
 
 namespace local = likely;
 
-local::BinnedGrid::BinnedGrid() { }
+local::BinnedGrid::BinnedGrid(std::vector<AbsBinningCPtr> axes)
+: _axisBinning(axes)
+{
+    if(0 == axes.size()) {
+        throw RuntimeError("BinnedGrid: no axes provided.");
+    }
+    _nbins = 1;
+    BOOST_FOREACH(AbsBinningCPtr binning, _axisBinning) {
+        _nbins *= binning->getNBins();
+    }
+}
 
 local::BinnedGrid::~BinnedGrid() { }
+
+int local::BinnedGrid::getIndex(std::vector<int> const &binIndices) const {
+    int nAxes(getNAxes());
+    if(binIndices.size() != nAxes) {
+        throw RuntimeError("BinnedGrid::getIndex: invalid input vector size.");
+    }
+    int index(0);
+    for(int axis = 0; axis < nAxes; ++axis) {
+        int binIndex(binIndices[axis]), nBins(_axisBinning[axis]->getNBins());
+        if(binIndex < 0 || binIndex >= nBins) {
+            throw RuntimeError("BinnedGrid::getIndex: invalid bin index.");
+        }
+        index = binIndex + index*nBins;
+    }
+    return index;
+}
+
+int local::BinnedGrid::getIndex(std::vector<double> const &values) const {
+    int index(0), nAxes(getNAxes());
+    if(values.size() != nAxes) {
+        throw RuntimeError("BinnedGrid::getIndex: invalid input vector size.");
+    }
+    std::vector<int> binIndices;
+    for(int axis = 0; axis < nAxes; ++axis) {
+        binIndices.push_back(_axisBinning[axis]->getBinIndex(values[axis]));
+    }
+    return getIndex(binIndices);
+}
+
+void local::BinnedGrid::getBinIndices(int index, std::vector<int> &binIndices) const {
+    _checkIndex(index);
+    int nAxes(getNAxes());
+    binIndices.resize(nAxes,0);
+    int partial(index);
+    for(int axis = nAxes-1; axis >= 0; --axis) {
+        AbsBinningCPtr binning = _axisBinning[axis];
+        int nBins(binning->getNBins()), binIndex(partial % nBins);
+        binIndices[axis] = binIndex;
+        partial = (partial - binIndex)/nBins;
+    }
+}
+
+void local::BinnedGrid::getBinCenters(int index, std::vector<double> &binCenters) const {
+    _checkIndex(index);
+    binCenters.resize(0);
+    binCenters.reserve(getNAxes());
+    std::vector<int> binIndices;
+    getBinIndices(index,binIndices);
+    int nAxes(getNAxes());
+    for(int axis = 0; axis < nAxes; ++axis) {
+        AbsBinningCPtr binning = _axisBinning[axis];
+        binCenters.push_back(binning->getBinCenter(binIndices[axis]));
+    }
+}
+
+void local::BinnedGrid::getBinWidths(int index, std::vector<double> &binWidths) const {
+    _checkIndex(index);
+    binWidths.resize(0);
+    binWidths.reserve(getNAxes());
+    std::vector<int> binIndices;
+    getBinIndices(index,binIndices);
+    int nAxes(getNAxes());
+    for(int axis = 0; axis < nAxes; ++axis) {
+        AbsBinningCPtr binning = _axisBinning[axis];
+        binWidths.push_back(binning->getBinWidth(binIndices[axis]));
+    }
+}
+
+void local::BinnedGrid::_checkIndex(int index) const {
+    if(index < 0 || index >= _nbins) {
+        throw RuntimeError("BinnedGrid: invalid index " +
+            boost::lexical_cast<std::string>(index));
+    }
+}
--- likely/BinnedGrid.h
+++ likely/BinnedGrid.h
@@ -3,13 +3,49 @@
 #ifndef LIKELY_BINNED_GRID
 #define LIKELY_BINNED_GRID
 
+#include "likely/types.h"
+
+#include <vector>
+
 namespace likely {
+    // Represents a multidimensional grid defined by an AbsBinning along each axis, and
+    // defines a mapping between a global integer index and the individual axis indices.
 	class BinnedGrid {
 	public:
-		BinnedGrid();
+	    // Creates a new grid with the specified binning for each axis.
+        explicit BinnedGrid(std::vector<AbsBinningCPtr> axes);
 		virtual ~BinnedGrid();
+		// Returns the number of axes for this grid.
+        int getNAxes() const;
+        // Returns the total number of bins covering the rectangular volume of this grid.
+        int getNBinsTotal() const;
+        // Returns the global index corresponding to the specified bin index values along
+        // each axis. The global index is defined as (i0*n1+i1)*n2+…) where ik, nk are
+        // the bin index and number of bins for axis k, respectively. The global
+        // index will always be >= 0 and < getNBinsTotal().
+        int getIndex(std::vector<int> const &binIndices) const;
+        // Returns the global index corresponding to the specified coordinate values along
+        // each axis.
+        int getIndex(std::vector<double> const &values) const;
+        // Fills the vector provided with the bin index values along each axis for the specified
+        // global index.
+        void getBinIndices(int index, std::vector<int> &binIndices) const;
+        // Fills the vector provided with the bin centers along each axis for the specified
+        // global index.
+        void getBinCenters(int index, std::vector<double> &binCenters) const;
+        // Fills the vector provided with the full bin widths along each axis for the specified
+        // global index.
+        void getBinWidths(int index, std::vector<double> &binWidths) const;
 	private:
+        // Throws a RuntimeError unless the specified global index is valid.
+        void _checkIndex(int index) const;
+        int _nbins;
+        std::vector<AbsBinningCPtr> _axisBinning;
 	}; // BinnedGrid
+
+    inline int BinnedGrid::getNAxes() const { return _axisBinning.size(); }
+    inline int BinnedGrid::getNBinsTotal() const { return _nbins; }
+
 } // likely
 
 #endif // LIKELY_BINNED_GRID
