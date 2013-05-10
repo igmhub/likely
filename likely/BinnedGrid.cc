2c5b21d45329646bd65a2353b81f55a3005513a1
David Kirkby <dkirkby@uci.edu>
2013-05-10 08:57:25 +0200

    Add skeleton new class BinnedGrid

---
+++ likely/BinnedGrid.cc
@@ -0,0 +1,9 @@
+// Created 10-May-2013 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "likely/BinnedGrid.h"
+
+namespace local = likely;
+
+local::BinnedGrid::BinnedGrid() { }
+
+local::BinnedGrid::~BinnedGrid() { }
---
+++ likely/BinnedGrid.h
@@ -0,0 +1,15 @@
+// Created 10-May-2013 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#ifndef LIKELY_BINNED_GRID
+#define LIKELY_BINNED_GRID
+
+namespace likely {
+	class BinnedGrid {
+	public:
+		BinnedGrid();
+		virtual ~BinnedGrid();
+	private:
+	}; // BinnedGrid
+} // likely
+
+#endif // LIKELY_BINNED_GRID
